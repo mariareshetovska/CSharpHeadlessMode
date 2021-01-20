362774ceb543ec424445784dcb8837ab2331c094
mariareshetovska <mresetovska@gmail.com>
2021-01-20 19:47:21 +0200

    Add project files.

---
+++ CSharpHeadlessMode/HeadlessModeTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHeadlessMode
+{
+    class HeadlessModeTest
+    {
+        IWebDriver driver;
+       
+        [SetUp]
+        public void Setup()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless");
+            driver = new ChromeDriver(options);
+            driver.Url = "https://www.opencart.com/";
+            driver.Manage().Window.Maximize();
+         
+        }
+        [Test]
+        public void searchTest()
+        {
+            IWebElement searchElement = driver.FindElement(By.CssSelector("li.dropdown"));
+            searchElement.Click();
+            IWebElement element = driver.FindElement(By.XPath("//a[text()='OpenCart Cloud']"));
+            element.Click();
+            string currentUrl = driver.Url;
+            Assert.AreEqual("https://www.opencart.com/index.php?route=cloud/landing", currentUrl);
+        }
+
+
+        [TearDown]
+        public void close()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
---
+++ CSharpHeadlessMode/RestSharpTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using RestSharp.Authenticators;
+using RestSharp.Serialization.Json;
+using System;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHeadlessMode
+{
+    [TestFixture]
+    public class RestSharpTest
+    {
+        private const string BASE_URL = "https://www.opencart.com/";
+        RestClient client = new RestClient(BASE_URL);
+
+        [Test]
+        public void restGetFeaturesTest()
+        {
+            RestRequest request = new RestRequest("index.php?route=cms/feature", Method.GET);
+            IRestResponse restResponse = client.Execute(request);
+            string responseData = restResponse.Content;
+            Assert.IsTrue(responseData.Contains("OpenCart Features"));
+        }
+
+        [Test]
+        public void restPostLoginTest()
+        {
+            var body = new
+            {
+                Email = "csharptest12@gmail.com",
+                Password = "c#147852"
+            };
+            
+            RestRequest request = new RestRequest("LOGIN", Method.POST); 
+            request.AddHeader("Content-Type", "application/json");
+            request.AddJsonBody(body);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+        }
+    }
+}
